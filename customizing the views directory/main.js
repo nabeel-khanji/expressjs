1e9d4d6084cf59d77cfcd4b2cee71e7f123cab8c
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-27 13:54:36 +0500

    Customize the views directory

---
+++ customizing the views directory/main.js
@@ -0,0 +1,14 @@
+import { dirname } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
+const setFilederName = path.join(__dirname, '/Screen');
+console.log(setFilederName);
+const app = express();
+const port = 8000;
+app.set('view engine', 'hbs');
+app.set('views', setFilederName);
+app.get('/', (req, res) => { res.render('index', { name: "Nabeel" }) });
+app.listen(port, () => { console.log(`listening to the port ${port}`); });
